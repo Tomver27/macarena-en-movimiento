170773e9f683e2a6072ac49f25793c40ee684615
Tomver <tomasv0127@gmail.com>
2026-06-17 13:42:48 -0500

    config: capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'co.edu.piloto.macarena',
+  appName: 'Macarena en Movimiento',
+  webDir: 'out'
+};
+
+export default config;
